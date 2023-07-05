89316019dd2b4ca0004c1d968f9b52286e45b7dc
HeoGwangYoung <heogwangyoung@gmail.com>
2023-07-05 15:57:49 +0900

    MySQL

---
+++ 버스/시외버스(2023-06-30).sql
@@ -0,0 +1,36 @@
+-- 시외버스 api를 받아와서 저장
+-- 정류장정보 테이블 생성
+--CREATE TABLE tbl_ter(
+    --pCode	    VARCHAR2(13)	PRIMARY KEY,
+    --pName	    nVARCHAR2(50)	NOT NULL,	
+    --pItem	    nVARCHAR2(20)	NOT NULL,	
+    --pIPrice	    NUMBER,
+    --pOPrice	    NUMBER		
+--);
+
+CREATE TABLE tbl_ter(
+    terId           VARCHAR2(10)     PRIMARY KEY,
+    terName         nVARCHAR2(30)     NOT NULL,
+    terRegion       nVARCHAR2(10),
+    terCoorX        VARCHAR2(30),
+    terCoorY        VARCHAR2(30)
+);
+
+CREATE TABLE tbl_TerLink(
+    tl_Id       VARCHAR2(20)    PRIMARY KEY,
+    depTerId        VARCHAR2(10)    NOT NULL,
+    arrTerId        VARCHAR2(10)    NOT NULL
+);
+
+CREATE TABLE tbl_TerDrive(
+    td_Id               VARCHAR2(20)    PRIMARY KEY,
+    tl_Id               VARCHAR2(20)    NOT NULL,
+    td_interval         VARCHAR2(6),
+    td_wasteTime        VARCHAR2(6),
+    td_fare             NUMBER
+);
+
+CREATE TABLE tbl_Terschedule(
+    td_Id               VARCHAR2(20)    NOT NULL,
+    tes_schedule        VARCHAR2(6)
+);
